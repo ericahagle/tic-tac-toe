905a78813df4c4fe23315f7c11f9020524bde742
Erica Hagle <ericahagle.dev@gmail.com>
2023-09-22 12:58:20 -0700

    feat: Add functionality to track which player's turn it is

--- main.js
+++ main.js
@@ -3,16 +3,32 @@ var playerOneSymbol = document.querySelector("#p1Symbol");
 var playerTwoSymbol = document.querySelector("#p2Symbol");
 var playerOneWins = document.querySelector("#p1Wins");
 var playerTwoWins = document.querySelector("#p2Wins");
+var whoseTurnItIs = document.querySelector("#turns");
+var gameBoard = document.querySelector("#gameBoard");
+var gameBoardCell1 = document.querySelector("#cell1");
+var gameBoardCell2 = document.querySelector("#cell2");
+var gameBoardCell3 = document.querySelector("#cell3");
+var gameBoardCell4 = document.querySelector("#cell4");
+var gameBoardCell5 = document.querySelector("#cell5");
+var gameBoardCell6 = document.querySelector("#cell6");
+var gameBoardCell7 = document.querySelector("#cell7");
+var gameBoardCell8 = document.querySelector("#cell8");
+var gameBoardCell9 = document.querySelector("#cell9");
 
 
 /* Data Model */
 var players = [];
+var whoseTurn;
 
 
 /* Event Listeners */
 window.addEventListener("load", function() {
   savePlayers();
+  whoseTurn = players[0];
   showPlayers();
+});
+gameBoard.addEventListener("click", function () {
+  trackTurns();
 });
 
 
@@ -46,4 +62,15 @@ function showPlayers() {
 
   playerTwoSymbol.innerHTML = players[1].token;
   playerTwoWins.innerHTML = `${players[1].wins} wins`;
+}
+
+function trackTurns() {
+  if (whoseTurn === players[0]) {
+    whoseTurn = players[1];
+    whoseTurnItIs.innerHTML = `It's ${players[1].token}'s turn`;
+  } else {
+    whoseTurn = players[0];
+    whoseTurnItIs.innerHTML = `It's ${players[0].token}'s turn`;
+  }
+  return whoseTurn;
 }
